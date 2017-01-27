be4e3d65051d03c1842a95964c9f5dd2ad54a68b
George Yu <dumpling_yu@163.com>
2017-01-27 15:24:31 +0800

    Format output (add __repr__)

--- Projects/fraction.py
+++ Projects/fraction.py
@@ -3,6 +3,9 @@ import math
 class Fraction(object):
     '''A fraction object'''
 
+    def __repr__(self):
+        return 'Fraction({} / {})'.format(self._numer,self._denom)
+
     def __init__(self, numerator, denominator):
         types = [type(numerator), type(denominator)]
         if not (types in ([float,float],[int,int]) or (float in types and int in types)):
@@ -17,11 +20,12 @@ class Fraction(object):
             floatDigit = max([str(self._numer)[::-1].find('.'), str(self._denom)[::-1].find('.')])
             self._numer = int(self._numer*10**floatDigit)
             self._denom = int(self._denom*10**floatDigit)
+        gcd = math.gcd(self._numer, self._denom)
+        self._numer, self._denom = self._numer//gcd, self._denom//gcd
         if division and (self._numer / self._denom).is_integer():
             return int(self._numer / self._denom)
-        gcd = math.gcd(self._numer, self._denom)
-        return self._numer//gcd, self._denom//gcd
+        return self._numer, self._denom
 
 #Example Usage:
-frac = Fraction(3,12)
+frac = Fraction(4,2)
 print(frac.simplify())
